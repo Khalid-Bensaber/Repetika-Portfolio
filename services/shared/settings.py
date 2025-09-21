bda7602e603ed6fe1ef6b094d6a55a7457bab0c5
Romain <romain.ryckebusch@student.junia.com>
2025-09-21 17:10:10 +0200

    Added throttle to django rest framework to prevent Denial of Service attacks

--- services/shared/settings.py
+++ services/shared/settings.py
@@ -132,6 +132,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+        "rest_framework.throttling.ScopedRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day",
+    },
 }
 
 SIMPLE_JWT = {
